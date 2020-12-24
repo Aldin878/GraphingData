f535cf3895fc8429cf1cb832a991497e4d3e074d
Aldin878 <61169135+Aldin878@users.noreply.github.com>
2020-12-23 21:04:07 -0600

    Update PartiesApplication.java

--- PartiesApplication.java
+++ PartiesApplication.java
@@ -8,30 +8,39 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
+import javafx.scene.chart.NumberAxis;
 
 public class PartiesApplication extends Application {
-    ArrayList<String> holder = new ArrayList<>();
-    String [] pieces;
-    
+
     @Override
     public void start(Stage window) {
-
-        try (Scanner scanner = new Scanner(Paths.get("partiesdata.tsv"))) {
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                
-                pieces = line.split("\t");
-                
-                holder.addAll(Arrays.asList(pieces));
-                
-            }
-        } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
-        } 
+        NumberAxis xAxis = new NumberAxis();
+        NumberAxis yAxis = new NumberAxis();
+        ArrayList<String> data = new ArrayList<>();
+        
     }
 
     public static void main(String[] args) {
         launch(PartiesApplication.class);
     }
 
+    public static ArrayList<String> getData() {
+        ArrayList<String> holder = new ArrayList<>();
+        String[] pieces;
+        
+        try (Scanner scanner = new Scanner(Paths.get("partiesdata.tsv"))) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+
+                pieces = line.split("\t");
+
+                holder.addAll(Arrays.asList(pieces));
+
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        
+        return holder;
+    }
 }
