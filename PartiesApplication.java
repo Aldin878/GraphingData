2446c78dbc67ce3b7d536704bc74f52c2e52fddb
Aldin878 <61169135+Aldin878@users.noreply.github.com>
2020-12-23 21:11:47 -0600

    Update PartiesApplication.java

--- PartiesApplication.java
+++ PartiesApplication.java
@@ -9,15 +9,24 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
 import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.LineChart;
 
 public class PartiesApplication extends Application {
 
     @Override
     public void start(Stage window) {
-        NumberAxis xAxis = new NumberAxis();
+        NumberAxis xAxis = new NumberAxis(1968, 2008, 4);
         NumberAxis yAxis = new NumberAxis();
-        ArrayList<String> data = new ArrayList<>();
+        ArrayList<String> data = getData();
+
+        xAxis.setLabel("Year");
+        yAxis.setLabel("Relative support (%)");
+
+        LineChart<Number, Number> lineChart = new LineChart<>(xAxis, yAxis);
+        lineChart.setTitle("Relative support in the years 1968-2008");
         
+        
+
     }
 
     public static void main(String[] args) {
@@ -27,7 +36,7 @@ public class PartiesApplication extends Application {
     public static ArrayList<String> getData() {
         ArrayList<String> holder = new ArrayList<>();
         String[] pieces;
-        
+
         try (Scanner scanner = new Scanner(Paths.get("partiesdata.tsv"))) {
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
@@ -40,7 +49,19 @@ public class PartiesApplication extends Application {
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
         }
-        
+
         return holder;
     }
+    
+    public static ArrayList<Integer> getYears(ArrayList<String> data) {
+        ArrayList<Integer> years = new ArrayList<>();
+        
+        for(int i = 1; i < 12; i++) {
+            years.add(Integer.valueOf(data.get(i)));
+        }
+        
+        
+        return years;
+    }
+    
 }
