8d00c2408354368ed2675d038f7057b3c0a07d4f
Aldin878 <61169135+Aldin878@users.noreply.github.com>
2021-01-17 23:48:37 -0600

    Update PartiesApplication.java

--- PartiesApplication.java
+++ PartiesApplication.java
@@ -1,58 +1,61 @@
-package application;
+package sample;
 
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import java.util.Scanner;
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Arrays;
-import javafx.scene.chart.NumberAxis;
+import javafx.stage.Stage;
 import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.chart.NumberAxis;
 import java.util.HashMap;
 
-public class PartiesApplication extends Application {
+import java.util.Arrays;
+import java.util.Scanner;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+public class Main extends Application {
 
     @Override
-    public void start(Stage window) {
-        NumberAxis xAxis = new NumberAxis(1968, 2008, 4);
-        NumberAxis yAxis = new NumberAxis();
-        ArrayList<String> data = getData();
-        ArrayList<Integer> years = getYears(data);
-        ArrayList<String> parties = getParties(data);
-        HashMap<String, HashMap<Integer, Double>> partySupport = getPartySupport(years, data);
+    public void start(Stage stage) throws Exception{
 
-        xAxis.setLabel("Year");
-        yAxis.setLabel("Relative support (%)");
 
-        LineChart<Number, Number> lineChart = new LineChart<>(xAxis, yAxis);
-        lineChart.setTitle("Relative support in the years 1968-2008");
-        ;
+
+
     }
 
+
     public static void main(String[] args) {
-        launch(PartiesApplication.class);
+        ArrayList<String> data = new ArrayList<>();
+        ArrayList<String> holder = new ArrayList<>();
+        ArrayList<Integer> intHolder = new ArrayList<>();
+        ArrayList<Double> doubleHolder = new ArrayList<>();
+
+        data = getData();
+        holder = getParties(data);
+        doubleHolder = getRatings(data);
+        
+        
+        
     }
 
     public static ArrayList<String> getData() {
-        ArrayList<String> holder = new ArrayList<>();
-        String[] pieces;
+        ArrayList<String> data = new ArrayList<>();
+        String line;
 
-        try (Scanner scanner = new Scanner(Paths.get("partiesdata.tsv"))) {
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
 
-                pieces = line.split("\t");
-
-                holder.addAll(Arrays.asList(pieces));
+        try(Scanner scanner = new Scanner(Paths.get("partiesdata.tsv"))) {
+            while(scanner.hasNextLine()) {
+                line = scanner.nextLine();
+                String[] pieces = line.split("\t");
+                data.addAll(Arrays.asList(pieces));
 
             }
-        } catch (Exception e) {
+        } catch(Exception e) {
             System.out.println("Error: " + e.getMessage());
         }
 
-        return holder;
+        return data;
     }
 
     public static ArrayList<Integer> getYears(ArrayList<String> data) {
@@ -75,33 +78,26 @@ public class PartiesApplication extends Application {
         return parties;
     }
 
-    public static HashMap<String, HashMap<Integer, Double>> getPartySupport(ArrayList<Integer> years, ArrayList<String> data) {
-        HashMap<Integer, Double> values = new HashMap<>();
-        HashMap<String, HashMap<Integer, Double>> partySupport = new HashMap<>();
-        ArrayList<String> names = new ArrayList<>();
+    public static ArrayList<Double> getRatings(ArrayList<String> data) {
+        ArrayList<Double> ratingData = new ArrayList<>();
         int count = 0;
 
-        for (int i = 0; i < years.size(); i++) {
-            for (int j = 0; j < data.size(); j++) {
-                if(data.get(i).equals("-")) {
-                    continue;
-                }
-                
-                Double value = Double.parseDouble(data.get(i));
-                
-                values.put(years.get(i), value);
+        for(int i = 13; i < data.size(); i++) {
+            if(count == 11) {
+                count = 0;
+                continue;
             }
-            
-            for (int k = 12; k < data.size(); k += 12) {
-                names.add(data.get(k));
+
+            if(data.get(i).equals("-")) {
+                count++;
+                continue;
             }
-            
-            for(String name : names) {
-                System.out.println(name);
-            }
+
+            ratingData.add(Double.valueOf(data.get(i)));
+            count++;
         }
 
-        return partySupport;
+        return ratingData;
     }
 
-}
+    }
